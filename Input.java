bc5255da2deb54056d4f0ce9162c93c8325776b2
Otaku <rudraanup01@gmail.com>
2026-09-23 16:36:48 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Input{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please tell me your name :]");
+        String name = sc.nextLine();
+        System.out.println("owo nice to meowt you..... "+name);
+    }
+}
---
+++ hey.java
@@ -0,0 +1,7 @@
+public class hey{
+    static void main(String[]args){
+        System.out.println("hello"+" this will be my first ever java program....");
+        System.out.println("hope everything goes well!");
+        System.out.println("<3 <3 <3");
+    }
+}
