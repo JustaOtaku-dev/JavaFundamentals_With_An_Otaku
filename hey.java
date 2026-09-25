476b8e054b58af938130c20611e9795b9bede590
Otaku <rudraanup01@gmail.com>
2026-09-25 23:57:46 +0530

    Delete hey.java

--- hey.java
+++
@@ -1,7 +0,0 @@
-public class hey{
-    static void main(String[]args){
-        System.out.println("hello"+" this will be my first ever java program....");
-        System.out.println("hope everything goes well!");
-        System.out.println("<3 <3 <3");
-    }
-}
