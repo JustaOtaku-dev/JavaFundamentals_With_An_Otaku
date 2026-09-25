1240dd67bc08c8d4cf1dcd27e948fdf237839427
Otaku <rudraanup01@gmail.com>
2026-09-25 23:57:04 +0530

    Delete ForLoop.java

--- ForLoop.java
+++
@@ -1,7 +0,0 @@
-public class ForLoop {
-    static void main(String[]args){
-        for(int counter = 0; counter < 67; counter = counter+1){
-            System.out.println("Hey there! Will u be my friend????? :}");
-        }
-    }
-}
